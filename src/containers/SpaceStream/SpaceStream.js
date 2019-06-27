01ead55090ad909c66f024656fac43e2e3b436e3
Mark McDaniels <mcdaniels.mark@gmail.com>
2019-06-26 22:44:09 -0400

    Setup
    Creating the structure, ejecting, and getting css modules.

---
+++ src/containers/SpaceStream/SpaceStream.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './SpaceStream.css';
+
+class SpaceStream extends Component {
+
+    render(){
+        return(
+            <div className="Test">
+                I'm in SpaceStream.
+            </div>
+        );
+    }
+
+}
+
+export default SpaceStream;
